3d368e6205572bad82d6ca86de0c76d7877f5eb8
SKM6607 <jksrikoushik@gmail.com>
2025-09-16 21:48:35 +0530

    add: more Logic
    version 0.0.2

--- src/Windows/StackWindow.kt
+++ src/Windows/StackWindow.kt
@@ -13,13 +13,17 @@ import javax.swing.SwingUtilities
 open class StackWindow : JPanel(), StackLightWeightInterface, GridInterface {
     val myWidth = StackLightWeightInterface.width;
     val myHeight = StackLightWeightInterface.height;
-    val nodeHeight = 75
-    val nodeWidth = 200
     private var top: VisualNode
     private var size = 0
-
+    val nodeHeight = 90
+    var nodeWidth = 100
+    val startX = returnClosest(myWidth / 6, myWidth / 4, SPACING + 5)
+    val endX = returnClosest(myWidth - 2*nodeWidth, myWidth - nodeWidth, SPACING +5)
+    val startY = nodeHeight * 2
+    val endY = returnClosest(myHeight - nodeHeight, myHeight, SPACING + 5)
     init {
-        top = VisualNode(0, myWidth / 2, myHeight - 3 * nodeHeight)
+        nodeWidth=endX-startX-60
+        top = VisualNode(0, startX + 30, (myHeight - 2 * nodeHeight))
         background = Color.BLACK
         preferredSize = Dimension(myWidth, myHeight)
     }
@@ -29,6 +33,7 @@ open class StackWindow : JPanel(), StackLightWeightInterface, GridInterface {
         val g: Graphics2D = g1 as Graphics2D
         drawGrid(g)
         drawBasket(g)
+        drawNode(g,top)
     }
 
     override fun push(value: Int) {
@@ -38,6 +43,7 @@ open class StackWindow : JPanel(), StackLightWeightInterface, GridInterface {
         newNode.nextAddress = top.address
         top = newNode
         size++
+        repaint()
     }
 
     override fun pop(): Int {
@@ -51,6 +57,7 @@ open class StackWindow : JPanel(), StackLightWeightInterface, GridInterface {
         //TODO: POSITION CHANGE
         top = temp
         size--
+        repaint()
         return retData
     }
 
@@ -63,7 +70,6 @@ open class StackWindow : JPanel(), StackLightWeightInterface, GridInterface {
     }
 
     override fun drawGrid(g: Graphics2D) {
-
         for (i in 0..myWidth step SPACING + 5) {
             g.drawLine(i, 0, i, myHeight)
         }
@@ -74,16 +80,21 @@ open class StackWindow : JPanel(), StackLightWeightInterface, GridInterface {
 
     final override fun drawNode(g: Graphics2D, node: VisualNode) {
         val oldColor = g.color
+        val resetStroke=g.stroke
         g.color = Color.ORANGE
         g.fillRect(node.xPos, node.yPos, nodeWidth, nodeHeight)
-        g.drawString("Data: $node.data", node.xPos + nodeWidth / 4, node.yPos - nodeHeight / 2)
-        g.drawLine(node.xPos + nodeWidth / 2, node.yPos, node.xPos + nodeWidth / 2, node.yPos - nodeHeight)
+        g.color= Color.WHITE
+        g.font=Font(Font.SANS_SERIF,Font.BOLD,25)
+        g.stroke= BasicStroke(6f)
+        g.drawString("Data: ${node.data}", node.xPos + nodeWidth / 4, node.yPos + nodeHeight / 2)
+        g.drawLine(node.xPos + nodeWidth / 2, node.yPos, node.xPos + nodeWidth / 2, node.yPos + nodeHeight)
         g.drawString(
-            "Address: $node.nextAddress",
+            "Address: ${node.nextAddress}",
             node.xPos + 3 * nodeWidth / 4,
-            node.yPos - nodeHeight / 2
+            node.yPos + nodeHeight / 2
         )
         g.color = oldColor
+        g.stroke=resetStroke
     }
 
     private fun returnClosest(startPoint: Int, endPoint: Int, divisor: Int): Int {
@@ -100,10 +111,6 @@ open class StackWindow : JPanel(), StackLightWeightInterface, GridInterface {
         val color = g.color
         g.stroke = BasicStroke(15f)
         g.color = DefaultWindowsInterface.themeColorBG
-        var startX = returnClosest(myWidth / 6, myWidth / 4, SPACING + 5)
-        val endX = returnClosest(myWidth - 2*nodeWidth, myWidth - nodeWidth, SPACING +5)
-        val startY = nodeHeight * 2
-        val endY = returnClosest(myHeight - nodeHeight, myHeight, SPACING + 5)
         g.drawLine(startX, startY, startX, endY)
         g.drawLine(startX, endY, endX, endY)
         g.drawLine(endX, endY, endX, startY)
