ea03dfd1e032dd4db1b94d9109a21475c9307577
SKM6607 <jksrikoushik@gmail.com>
2026-02-20 18:18:33 +0530

    add: Searching Window

--- src/main/interfaces/MacroInterface.java
+++ src/main/interfaces/MacroInterface.java
@@ -14,13 +14,13 @@ public interface MacroInterface extends DefaultWindowsInterface {
     String SELECTION_SORTING = "Selection Sort";
     String BUBBLE_SORTING = "Bubble Sort";
     String QUICK_SORTING = "Quick Sort";
-    String[] SORTING_ARRAY = {SELECTION_SORTING, INSERTION_SORTING, BUBBLE_SORTING, QUICK_SORTING};
-    String[] QUEUE_ARRAY = {SIMPLE_QUEUE, CIRCULAR_QUEUE, PRIORITY_QUEUE};
-    String[] LINKED_LIST_ARRAY = {LINKED_LIST, CYCLE_DETECTION};
-    String[] STACK_ARRAY = {STACK};
     String SEARCHING_ALGORITHMS = "Searching Algorithms";
     String A_STAR_ALGORITHM = "A* Algorithm";
     String BFS_ALGORITHM = "Breadth First Search";
     String DFS_ALGORITHM = "Depth First Search";
+    String[] SORTING_ARRAY = {SELECTION_SORTING, INSERTION_SORTING, BUBBLE_SORTING, QUICK_SORTING};
+    String[] QUEUE_ARRAY = {SIMPLE_QUEUE, CIRCULAR_QUEUE, PRIORITY_QUEUE};
+    String[] LINKED_LIST_ARRAY = {LINKED_LIST, CYCLE_DETECTION};
+    String[] STACK_ARRAY = {STACK};
     String[] SEARCHING_ARRAY = {BFS_ALGORITHM, DFS_ALGORITHM, A_STAR_ALGORITHM};
 }
--- src/main/searching/SearchingWindow.java
+++ src/main/searching/SearchingWindow.java
@@ -1,16 +1,77 @@
 package main.searching;
-
 import javax.swing.*;
 import java.awt.*;
-public abstract class SearchingWindow
-        extends JPanel {
+public abstract class SearchingWindow extends JPanel {
+    private static final String SELECTED_NODE = "Selected Node: ";
+    private static final String ADD_NODE = "Add Node";
+    private static final String DELETE_NODE = "Delete Node";
+    private static final String GOAL_STATE = "Set Goal State";
+    private static final String START_SEARCHING = "Start Searching";
+    private final String algorithmTitle;
+    private final JLabel nodeSelectedLabel = new JLabel("");
+    private final JTextField valueToAddTextField = new JTextField("");
+    private final JButton addNodeButton = new JButton(ADD_NODE);
+    private final JButton deleteNodeButton = new JButton(DELETE_NODE);
+    private final JButton setGoalStateButton = new JButton(GOAL_STATE);
+    private final JButton beginSearchButton = new JButton(START_SEARCHING);
     protected SearchingVisual searchingVisual;
+    protected SearchingAlgorithm searchingAlgorithm;
     protected JPanel controlPanel;
 
     protected SearchingWindow(SearchingVisual searchingVisual) {
         this.searchingVisual = searchingVisual;
+        this.searchingAlgorithm = searchingVisual.algorithm;
+        this.algorithmTitle = this.searchingAlgorithm.algorithmName;
+        this.controlPanel = setupControlPanel();
+        this.setupActionListeners();
         add(searchingVisual, BorderLayout.CENTER);
         add(controlPanel, BorderLayout.SOUTH);
     }
+
+    private void setConstraints(GridBagConstraints gB, int... c) {
+        gB.gridx = c[0];
+        gB.gridy = c[1];
+        if (c.length == 3)
+            gB.gridwidth = c[2];
+    }
+
+    private JPanel setupControlPanel() {
+        if (this.controlPanel != null) return null;
+        JPanel controlPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        JLabel titleOfAlgorithm = new JLabel(algorithmTitle);
+        setConstraints(gridBagConstraints, 0, 0, 3);
+        controlPanel.add(titleOfAlgorithm, gridBagConstraints);
+        JLabel selectedNode = new JLabel(SELECTED_NODE);
+        setConstraints(gridBagConstraints, 1, 0, 1);
+        controlPanel.add(selectedNode, gridBagConstraints);
+        setConstraints(gridBagConstraints, 1, 1, 1);
+        controlPanel.add(nodeSelectedLabel, gridBagConstraints);
+        setConstraints(gridBagConstraints, 1, 2, 1);
+        controlPanel.add(valueToAddTextField, gridBagConstraints);
+        setConstraints(gridBagConstraints, 1, 3, 1);
+        controlPanel.add(addNodeButton, gridBagConstraints);
+        setConstraints(gridBagConstraints, 2, 0, 1);
+        controlPanel.add(deleteNodeButton, gridBagConstraints);
+        setConstraints(gridBagConstraints, 2, 1, 1);
+        controlPanel.add(setGoalStateButton, gridBagConstraints);
+        setConstraints(gridBagConstraints, 2, 2, 1);
+        controlPanel.add(beginSearchButton, gridBagConstraints);
+        return controlPanel;
+    }
+
+    private void setupActionListeners() {
+        valueToAddTextField.addActionListener(e -> {
+        });
+        addNodeButton.addActionListener(a -> {
+        });
+        deleteNodeButton.addActionListener(a -> {
+        });
+        setGoalStateButton.addActionListener(a -> {
+        });
+        beginSearchButton.addActionListener(a -> {
+        });
+    }
+
 
 }
